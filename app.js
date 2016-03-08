d590dec596429e755bd0e281fd82437999e794df
Zibi Work <zbigniew.pytel@gmail.com>
2016-03-08 13:36:08 -0500

    added storage option to multer

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var multer = require("multer");
 var app = express();
- 
+var storage=multer.diskStorage({
+    destination:function(req,file,cb){
+    cb(null,'./uploads')
+},
+filename:function(req,file,cb){
+    cb(null, Date.now() + file.originalname)
+}
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
@@ -12,8 +20,9 @@ app.use(function(req, res, next) {
     next();
 });
  
-app.post("/upload", multer({dest: "./uploads/"}).array("uploads", 12), function(req, res) {
+app.post("/upload", multer({storage:storage}).array("uploads[]", 12), function(req, res) {
     res.send(req.files);
+    console.log(req.files)
 });
  
 var server = app.listen(3000, function() {
